045b8af0b6c96aca04e044e07483a0779062bc0e
NEDIX <2658411+nedix@users.noreply.github.com>
2024-04-17 17:18:14 +0200

    Fix buffer logging

--- rootfs/opt/bitcoin/rotating-logger.sh
+++ rootfs/opt/bitcoin/rotating-logger.sh
@@ -6,17 +6,16 @@ while IFS= read -r LINE; do
     echo "$LINE"
 
     if [ -z "$TIMEOUT" ]; then
+        LOG_LINES=$(cat "$LOG_FILE")
         TIMEOUT=$(( $(date +%s) + 1 ))
     fi
 
     if [ $(date +%s) -lt "$TIMEOUT" ]; then
-        LOG_LINES="${LOG_LINES:-}\n${LINE}"
+        LOG_LINES="${LOG_LINES}\n${LINE}"
         continue
     fi
 
-    (cat "$LOG_FILE"; echo -n "$LOG_LINES") \
-        | tail -n 100 \
-        | sed -e "w ${LOG_FILE}"
+    echo -n "$LOG_LINES" | tail -n 100 | sed -e "w ${LOG_FILE}" > /dev/null 2>&1
 
     LOG_LINES=""
     TIMEOUT=""
